5348b1feed8a73b68bb0106ae1688e2ff426e657
Clecio Jung <clecio.jung@gmail.com>
2020-02-18 16:20:17 -0300

    Improved main argument parsing and interpreter

--- Makefile
+++ Makefile
@@ -1,2 +1,101 @@
-all:
-	gcc -o BrainFuckInterpreter main.c
+# ----------------------------------------
+# Project definitions
+# ----------------------------------------
+
+# Name of the project
+EXEC = BrainFuckInterpreter
+
+# Folders
+ODIR = .obj
+DDIR = .deps
+SDIR = src
+IDIR = src
+
+# .c files
+SRCS = $(wildcard $(SDIR)/*.c $(SDIR)/*.cpp)
+
+# .h files
+INCS = $(wildcard $(IDIR)/*.h $(IDIR)/*.hpp)
+
+# Dependency files (auto generated)
+DEPS = $(patsubst %,%.d,$(basename $(subst $(SDIR),$(DDIR),$(SRCS))))
+
+# Object files
+OBJS = $(patsubst %,%.o,$(basename $(subst $(SDIR),$(ODIR),$(SRCS))))
+
+# ----------------------------------------
+# Compiler and linker definitions
+# ----------------------------------------
+
+ # Compiler and linker
+#CC = gcc
+#CXX = g++
+
+# Libraries
+INCLUDES =
+
+# Flags for compiler
+CFLAGS = -W -Wall -Wextra -pedantic -std=c99 -O2
+CXXFLAGS = -W -Wall -Wextra -pedantic -O2
+DEPFLAGS = -MT $@ -MMD -MP -MF $(DDIR)/$*.Td
+
+# Compiler macros
+COMPILE.CC = $(CC) $(CFLAGS)
+COMPILE.CXX = $(CXX) $(CXXFLAGS)
+POSTCOMPILE = mv -f $(DDIR)/$*.Td $(DDIR)/$*.d && touch $@
+
+# ----------------------------------------
+# Fomating macros
+# ----------------------------------------
+
+BOLD = \033[1m
+NORMAL = \033[0m
+RED = \033[0;31m
+GREEN = \033[0;32m
+
+# ----------------------------------------
+# Compilation and linking rules
+# ----------------------------------------
+
+all: $(EXEC)
+
+$(EXEC): $(OBJS)
+	@ echo "${GREEN}Building binary: ${BOLD}$@${GREEN} using dependencies: ${BOLD}$^${NORMAL}"
+	$(COMPILE.CC) $(filter %.s %.o,$^) -o $@ $(INCLUDES)
+	@ touch $@
+
+$(ODIR)/%.o : $(SDIR)/%.c
+$(ODIR)/%.o : $(SDIR)/%.c $(DDIR)/%.d | $(DDIR) $(ODIR)
+	@ echo "${GREEN}Building target: ${BOLD}$@${GREEN}, using dependencies: ${BOLD}$^${NORMAL}"
+	$(COMPILE.CC) $(DEPFLAGS) -c $(filter %.c %.s %.o,$^) -o $@
+	@ $(POSTCOMPILE)
+
+$(ODIR)/%.o : $(SDIR)/%.cpp
+$(ODIR)/%.o : $(SDIR)/%.cpp $(DDIR)/%.d | $(DDIR) $(ODIR)
+		@ echo "${GREEN}Building target: ${BOLD}$@${GREEN}, using dependencies: ${BOLD}$^${NORMAL}"
+		$(COMPILE.CXX) $(DEPFLAGS) -c $(filter %.cpp %.s %.o,$^) -o $@
+		@ $(POSTCOMPILE)
+
+$(DDIR)/%.d: ;
+.PRECIOUS: $(DDIR)/%.d
+
+-include $(DEPS)
+
+# ----------------------------------------
+# Script rules
+# ----------------------------------------
+
+$(DDIR):
+	mkdir -p $@
+
+$(ODIR):
+	mkdir -p $@
+
+clean:
+	rm -fr $(ODIR)/ $(DDIR)/ $(EXEC) $(SDIR)/*.gch *~ env.mk
+
+remade: clean all
+
+.PHONY: all clean remade
+
+# ----------------------------------------
---
+++ src/main.c
@@ -0,0 +1,151 @@
+//------------------------------------------------------------------------------
+// LIBRARIES
+//------------------------------------------------------------------------------
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+#include "arguments.h"
+#include "parser.h"
+
+//------------------------------------------------------------------------------
+// USER TYPES
+//------------------------------------------------------------------------------
+
+enum ActionToBeTaken {
+  acNone = 0,
+  acParser,
+};
+
+//------------------------------------------------------------------------------
+// GLOBAL VARIABLES
+//------------------------------------------------------------------------------
+
+static const char *const version = "1.0.0";
+
+static const char *fileName = NULL;
+
+static enum ActionToBeTaken action = acParser;
+
+//------------------------------------------------------------------------------
+// FUNCTIONS
+//------------------------------------------------------------------------------
+
+char *getContentFromFile(const char *const name) {
+  char *buffer = NULL;
+
+  // Get file size
+  struct stat st;
+  if(!stat(name, &st)) {
+    const size_t fileSize = st.st_size;
+
+    // Open file
+    FILE *file = fopen(name, "rb");
+    if (file) {
+      // Allocate memory to store the entire file
+      buffer = (char *) malloc((fileSize+1)*sizeof(char));
+      if (buffer) {
+        // Copy the contents of the file to the buffer
+        const size_t result = fread(buffer, sizeof(char), fileSize, file);
+        buffer[fileSize] = '\0';
+        if (result != fileSize) {
+          // Reading file error, free dinamically allocated memory
+          free(buffer);
+          buffer = NULL;
+        }
+      }
+      fclose(file);
+    }
+  }
+  return buffer;
+}
+
+static int printUsage(const char *const software) {
+  printf("[Usage] %s [script.b] [Options]\n", software);
+  action = acNone;
+  return EXIT_SUCCESS;
+}
+
+static int getFileName(const char *const arg) {
+  if (fileName) {
+    return EXIT_FAILURE;
+  } else {
+    fileName = arg;
+  }
+  return EXIT_SUCCESS;
+}
+
+static int printVersion(const char *const arg) {
+  (void) arg;
+  printf("[Version] %s\n", version);
+  action = acNone;
+  return EXIT_SUCCESS;
+}
+
+static int debugModeOn(const char *const arg) {
+  (void) arg;
+  printf("Debug mode on.\n");
+  debugMode = 1;
+  return EXIT_SUCCESS;
+}
+
+static int printLanguageInstructions(const char *const arg) {
+  (void) arg;
+  printInstructions();
+  action = acNone;
+  return EXIT_SUCCESS;
+}
+
+static int changeMemorySize(const char *const arg) {
+  unsigned int size;
+  const char * ptr = strchr(arg, '=');
+  size = atoi(++ptr);
+  if (size > 10) {
+    printf("Program buffer size changed to %u elements.\n", size);
+    memorySize = size;
+  }
+  return EXIT_SUCCESS;
+}
+
+//------------------------------------------------------------------------------
+// MAIN
+//------------------------------------------------------------------------------
+
+int main(const int argc, const char *const argv[])
+{
+  const char *prog;
+
+  initBrainFuck();
+
+  // Treat main() arguments
+  initArguments(printUsage, getFileName);
+  addArgument("--version", "-v", printVersion, "Display the software version.");
+  addArgument("--debug", "-d", debugModeOn, "Activate the debug mode (allows the use of the commands # and @).");
+  addArgument("--language", "-l", printLanguageInstructions, "Displays language instructions.");
+  addArgument("--memory=%d", "-m=%d", changeMemorySize, "Change program buffer size.");
+  parseArguments(argc, argv);
+
+  if (action == acNone) {
+    return EXIT_SUCCESS;
+  }
+
+  if (!fileName) {
+    argumentsUsage("No file specified");
+    return EXIT_FAILURE;
+  }
+
+  prog = getContentFromFile(fileName);
+  if (prog) {
+    brainFuck(prog);
+    free((char *) prog);
+  } else {
+    fprintf(stderr, "\n[Error]: Couldn't open the file %s\n", fileName);
+    return EXIT_FAILURE;
+  }
+  return EXIT_SUCCESS;
+}
+
+//------------------------------------------------------------------------------
+// END
+//------------------------------------------------------------------------------
